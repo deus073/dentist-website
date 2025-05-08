de5335fcd7f27a4977f3eb9a33cd23e8e195e6e7
deus073 <deus_073@yahoo.com>
2025-05-08 17:35:56 +0300

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from 'react';
+import dynamic from 'next/dynamic';
+import TopBar from '@/components/layout/TopBar';
+import Navbar from '@/components/layout/Navbar';
+import StickyCallBar from '@/components/layout/StickyCallBar';
+import HeroSection from '@/components/sections/HeroSection';
+import ServicesSection from '@/components/sections/ServicesSection';
+import TestimonialsSection from '@/components/sections/TestimonialsSection';
+import LocationSection from '@/components/sections/LocationSection';
+import Footer from '@/components/layout/Footer';
+import ScrollToTop from '@/components/layout/ScrollToTop';
+
+export default function Home() {
+  return (
+    <main>
+      <TopBar />
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <TestimonialsSection />
+      <LocationSection />
+      <Footer />
+      <StickyCallBar />
+      <ScrollToTop />
+    </main>
+  );
+}
